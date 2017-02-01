81ac3e6f92382163dcc6293c8139e98d28d8bd6b
Wen Kokke <wenkokke@users.noreply.github.com>
2017-02-01 16:22:37 +0000

    moved data to data subdir

--- plots.py
+++ plots.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python2.7
 
-from math import log
+from itertools import groupby
 from matplotlib import pyplot as plt
-from matplotlib.ticker import FuncFormatter
-from mpl_toolkits.mplot3d import Axes3D
 import re
 
 def chunk(l, n):
@@ -18,31 +16,30 @@ def parse_conf(conf):
 
 with open("try-parameters.log", 'r') as data_file:
     data = chunk(data_file.readlines(), 3)
-    data = [(parse_conf(conf), float(time), abs(float(score)))
+    data = [(parse_conf(conf), float(time), float(score))
             for (conf, time, score) in data]
-    
-    min_time = min(time for (_, time, _) in data)
-    max_time = max(time for (_, time, _) in data)
-    dlt_time = max_time - min_time
-    
-    min_score = min(score for (_, _, score) in data)
-    max_score = max(score for (_, _, score) in data)
-    dlt_score = max_score - min_score
-
-    data = [(conf,
-             ((time - min_time) / dlt_time * 100.0),
-             ((1.0 - ((score - min_score) / dlt_score))) * 100.0)
-            for (conf, time, score) in data]
+    data = groupby(data, key=lambda x: x[0][1])
 
 
-fig = plt.figure('fig-conf-time-score')
-ax = fig.add_subplot(111, projection='3d')
+plt.figure(1)
+plt.subplot(211)
+plt.xscale('log')
+plt.ylabel('time in ms')
+plt.subplot(212)
+plt.xscale('log')
+plt.ylabel('score')
 
-for ((s, k), time, score) in data:
-    s = log(s)
-    k = log(k)
-    ax.plot([ s, s ], [ k, k ], [ time, score ], '-')
-    ax.scatter([ s ], [ k ], [ time ], color='black')
-    ax.scatter([ s ], [ k ], [ score ], color='grey')
+for (i, group) in data:
+    confs, times, scores = zip(*group)
+    ss, ks = zip(*confs)
+    lbl = 'k=' + str(ks[0])
 
-fig.savefig('fig-conf-time-score.png')
+    plt.subplot(211)
+    plt.plot(ss, times, '-', label=lbl)
+    plt.subplot(212)
+    plt.plot(ss, scores, '-', label=lbl)
+
+plt.subplot(212)
+plt.legend()
+
+plt.savefig('fig-time-score')
